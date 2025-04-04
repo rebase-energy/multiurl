b8d5abe223735a1882e1e552b94642330637f881
Mihai Chiru <mihai@rebase.energy>
2025-04-04 15:01:01 +0200

    Retry on AssertionError

--- multiurl/http.py
+++ multiurl/http.py
@@ -482,6 +482,7 @@ def robust(call, maximum_tries=500, retry_after=120, mirrors=None):
             except (
                 requests.exceptions.ConnectionError,
                 requests.exceptions.ReadTimeout,
+                AssertionError,
             ) as e:
                 r = None
                 LOG.warning(
